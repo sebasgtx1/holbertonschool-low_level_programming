9db7fc36b3f4e1cf8fb26a20c8ec31274961e0e2
SebasGTX1 <sebas1355840289@LAPTOP-1EFK09F7.localdomain>
2022-02-10 12:14:09 -0500

    Task 5 done

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main_2 - prints a messge
+ *
+ * Return: Always 0 (Success)
+ */
+int main_2(void)
+
+{
+
+printf("with proper grammar, but the outcome is a piece of art,");
+
+return (0);
+
+}
