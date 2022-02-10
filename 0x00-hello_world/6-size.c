d934cac9e2bae672d765cebf6d0920d376800f88
SebasGTX1 <sebas1355840289@LAPTOP-1EFK09F7.localdomain>
2022-02-10 13:00:11 -0500

    checker test task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,11 +14,11 @@ long int longintType;
 long long int llintType;
 float floatType;
 
-printf("Size of char: %zu bytes\n", sizeof(charType));
-printf("Size of int: %zu bytes\n", sizeof(intType));
-printf("Size of long int: %zu bytes\n", sizeof(longintType));
-printf("Size of long long int: %zu byte\n", sizeof(llintType));
-printf("Size of long float: %zu bytes\n", sizeof(floatType));
+printf("Size of char: %zu byte(s)\n", sizeof(charType));
+printf("Size of int: %zu byte(s)\n", sizeof(intType));
+printf("Size of long int: %zu byte(s)\n", sizeof(longintType));
+printf("Size of long long int: %zu byte(s)\n", sizeof(llintType));
+printf("Size of long float: %zu byte(s)\n", sizeof(floatType));
 
 return (0);
 
