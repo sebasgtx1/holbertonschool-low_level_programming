f6cce1f11dae3ba009c75492b3a1c9ab4e3843cc
SebasGTX1 <sebas1355840289@LAPTOP-1EFK09F7.localdomain>
2022-02-10 12:35:57 -0500

    fixing task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - prints a messge
+ * main - prints a messge with printf
  *
  * Return: Always 0 (Success)
  */
@@ -9,7 +9,7 @@ int main(void)
 
 {
 
-printf("with proper grammar, but the outcome is a piece of art, ");
+printf("with proper grammar, but the outcome is a piece of art,");
 
 return (0);
 
