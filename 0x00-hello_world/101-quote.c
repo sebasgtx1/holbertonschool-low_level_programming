1e7a6f9a211c8dce5299f02b79ff3e38a8f70f84
SebasGTX1 <sebas1355840289@LAPTOP-1EFK09F7.localdomain>
2022-02-10 15:58:27 -0500

    advanced task 2 done

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints to the stderr
+ *
+ * Return: Always 1 (Success)
+ */
+
+int main(void)
+
+{
+
+fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58, 1, stderr);
+
+return (1);
+
+}
