e7208841b63dbbf49fe8167f9090112251fbdaf8
SebasGTX1 <sebas1355840289@LAPTOP-1EFK09F7.localdomain>
2022-02-11 11:31:37 -0500

    Task 5 complete

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints a messge with printf
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int c;
+
+for (c = 0; c < 10; ++c)
+{
+printf("%i", c);
+}
+return (0);
+}
