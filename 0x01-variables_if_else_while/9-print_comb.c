e507d6ee6c470a66711bf127b9dfcab173ce8651
SebasGTX1 <sebas1355840289@LAPTOP-1EFK09F7.localdomain>
2022-02-11 13:33:05 -0500

    Task 9, complete

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints a messge with printf
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int c;
+
+for (c = 0; c < 10; ++c)
+{
+putchar(c + ',');
+}
+putchar('\n');
+return (0);
+}
