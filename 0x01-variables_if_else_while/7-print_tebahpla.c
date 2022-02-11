ccb67dd0a3011edd83a156688ae1cc1b31360b18
SebasGTX1 <sebas1355840289@LAPTOP-1EFK09F7.localdomain>
2022-02-11 13:15:07 -0500

    Task 7, complete

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints a messge with printf
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char c;
+for (c = 'z'; c >= 'a'; --c)
+{
+putchar(c);
+}
+putchar('\n');
+return (0);
+}
