bb7971729f96498f7091de7aa92480d7016db18b
SebasGTX1 <sebas1355840289@LAPTOP-1EFK09F7.localdomain>
2022-02-11 10:59:10 -0500

    Task 2 done, use putchar and a for loop to prints the alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints a messge with printf
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char c;
+
+for (c = 'a'; c <= 'z'; ++c)
+{
+putchar(c);
+}
+return (0);
+}
