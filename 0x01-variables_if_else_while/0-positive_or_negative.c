56a27ac42f286fc206cc5e125a6e2b5c9bbe7571
SebasGTX1 <sebas1355840289@LAPTOP-1EFK09F7.localdomain>
2022-02-11 10:38:57 -0500

    Task 0 and Task 1 Betty Style and compilation fix

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,6 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
-
+#include <stdio.h>
 /**
  * main - prints a messge with printf
  *
@@ -15,12 +15,12 @@ n = rand() - RAND_MAX / 2;
 
 if (n < 0)
 {
-printf("%i is negative", n)
+printf("%i is negative\n", n);
 }
 
 else
 {
-printf("%i is positive", n)
+printf("%i is positive\n", n);
 }
 
 return (0);
