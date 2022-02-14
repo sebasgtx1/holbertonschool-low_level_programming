74c986c6aab10497b24ef54639f7df8375cdc3cf
SebasGTX1 <sebas1355840289@LAPTOP-1EFK09F7.localdomain>
2022-02-14 11:46:09 -0500

    Task 7 complete

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * print_last_digit -  print the last digit of a number
+ * @a: is a number
+ * Return: 0 (Success)
+*/
+int print_last_digit(int a)
+{
+printf("%i", a % 10);
+return (0);
+}
