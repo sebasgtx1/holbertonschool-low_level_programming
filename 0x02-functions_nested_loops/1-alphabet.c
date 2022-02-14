30d28a41d85374183da0542dadc49bb76fa3ebad
SebasGTX1 <sebas1355840289@LAPTOP-1EFK09F7.localdomain>
2022-02-14 10:24:40 -0500

    Task 1 complete

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 
 /**
- *  print_alphabet - prints the alphabet in lowercase 
+ * print_alphabet - prints the alphabet in lowercase
  *
  * Return: Always 0 (Success)
  */
-
-void print_alphabet(void);
+int print_alphabet(void)
 {
 char c;
 for (c = 'a'; c <= 'z'; ++c)
