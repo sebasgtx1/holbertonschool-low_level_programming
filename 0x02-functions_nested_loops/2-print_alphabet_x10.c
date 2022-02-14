70b5800ab0929b4362313916ce998ff1b8b976af
SebasGTX1 <sebas1355840289@LAPTOP-1EFK09F7.localdomain>
2022-02-14 10:30:55 -0500

    Task 2 complete

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * print_alphabet_x10 - prints the alphabet in lowercase x 10
+ *
+ *
+ */
+void print_alphabet_x10(void)
+{
+char c, i;
+for (i = 0 ; i = 10 ; i++)
+{
+for (c = 'a'; c <= 'z'; ++c)
+{
+putchar(c);
+}
+putchar('\n');
+}
+}
