63a2546b3b09ab43edc6216949e67244af37e01e
SebasGTX1 <sebas1355840289@LAPTOP-1EFK09F7.localdomain>
2022-02-14 10:18:00 -0500

    Task 1 complete

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *  print_alphabet - prints the alphabet in lowercase 
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void);
+{
+char c;
+for (c = 'a'; c <= 'z'; ++c)
+{
+putchar(c);
+}
+putchar('\n');
+return (0);
+}
