9c20b3d2175ba05e6f5c6e654cfc46cfb5e3a689
SebasGTX1 <sebas1355840289@LAPTOP-1EFK09F7.localdomain>
2022-02-14 11:23:33 -0500

    Task 6 complete

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _abs -  checks for lowercase character
+ * @a: is a number
+ * Return: 0 (Success)
+*/
+int _abs(int a)
+{
+printf("%i", abs(a));
+return (0);
+}
