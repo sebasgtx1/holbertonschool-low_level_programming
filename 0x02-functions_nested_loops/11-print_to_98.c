43f926b701d2215438e7e144a972728bd3616378
SebasGTX1 <sebas1355840289@LAPTOP-1EFK09F7.localdomain>
2022-02-14 13:39:39 -0500

    task 11 complete

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * print_to_98 - prints number until 98
+ * @a: number
+ * Return: 0 (Succes)
+ */
+int print_to_98(int a)
+{
+int i;
+
+if (a > 98)
+{
+for (i = a; i = 98; i--)
+{
+printf("%i, ", i);
+}
+}
+else
+{
+for (i = a; i = 98; i++)
+{
+printf("%i, ", i);
+}
+}
+return (0);
+}
