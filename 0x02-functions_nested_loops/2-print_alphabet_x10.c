d22225b792514854588df5f2076ee993fb25fba1
SebasGTX1 <sebas1355840289@LAPTOP-1EFK09F7.localdomain>
2022-02-14 10:32:59 -0500

    Task 2 complete

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,7 +3,7 @@
 /**
  * print_alphabet_x10 - prints the alphabet in lowercase x 10
  *
- *
+ * Return: Always 0 (Success)
  */
 void print_alphabet_x10(void)
 {
@@ -16,4 +16,5 @@ putchar(c);
 }
 putchar('\n');
 }
+return (0);
 }
