0c2039146c4d70f55949c9249da4c2a919fcbeee
SebasGTX1 <sebas1355840289@LAPTOP-1EFK09F7.localdomain>
2022-02-15 09:37:57 -0500

    task 5 fixed

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,18 +10,18 @@ int print_sign(int n)
 
 if (n > 0)
 {
-putchar(43);
 return (1);
+putchar(43);
 }
 else if (n < 0)
 {
-putchar(45);
 return (-1);
+putchar(45);
 }
-else 
+else
 {
-putchar(48);
 return (0);
+putchar(48);
 }
 putchar('\n');
 }
