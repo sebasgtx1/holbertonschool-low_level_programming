0293fd19a2399abc0b37b50697105ae8bbee4c56
SebasGTX1 <sebas1355840289@LAPTOP-1EFK09F7.localdomain>
2022-02-14 20:52:10 -0500

    only putchar

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * print_to_98 - prints number until 98
  * @a: number
@@ -7,20 +6,167 @@
  */
 int print_to_98(int a)
 {
-int i;
-
-if (a > 98)
+int i, d1, d2, d3;
+i = 0;
+if (a <= -10)
 {
-for (i = a; i = 98; i--)
+for (i = a; i <= 98; i++)
 {
-printf("%i, ", i);
+if (i == 98)
+{
+d2 = i % 10;
+d1 = i / 10;
+putchar('-');
+putchar(d1 + '0');
+putchar(d2 + '0');
+break;
+}
+d2 = i % 10;
+d1 = i / 10;
+putchar('-');
+putchar(d1 + '0');
+putchar(d2 + '0');
+putchar(',');
+putchar(' ');
 }
 }
-else
+if (a < 0 && a > -10)
 {
-for (i = a; i = 98; i++)
+for (i = a; i <= 98; i++)
 {
-printf("%i, ", i);
+if (i == 98)
+{
+putchar('-');
+putchar(i + '0');
+break;
+}
+putchar('-');
+putchar(-i + '0');
+putchar(',');
+putchar(' ');
+}
+}
+if (a >= 0 && a < 10)
+{
+for (i = a; i < 10; i++)
+{
+if (i == 98)
+{
+putchar(i + '0');
+break;
+}
+putchar(i + '0');
+putchar(',');
+putchar(' ');
+}
+}
+if (a >= 10 && a <= 98)
+{
+for (i = a; i <= 98; i++)
+{
+if (i == 98)
+{
+d2 = i % 10;
+d1 = i / 10;
+putchar(d1 + '0');
+putchar(d2 + '0');
+a = 0;
+i = 0;
+break;
+}
+d2 = i % 10;
+d1 = i / 10;
+putchar(d1 + '0');
+putchar(d2 + '0');
+putchar(',');
+putchar(' ');
+}
+}
+if (i >= 10 && i <= 98)
+{
+a = i;
+for (i = a; i <= 98; i++)
+{
+if (i == 98)
+{
+d2 = i % 10;
+d1 = i / 10;
+putchar(d1 + '0');
+putchar(d2 + '0');
+break;
+}
+d2 = i % 10;
+d1 = i / 10;
+putchar(d1 + '0');
+putchar(d2 + '0');
+putchar(',');
+putchar(' ');
+}
+}
+if (a > 98 && a < 100 || i < 100 && i > 98)
+{
+for (i = a; i >= 98; i--)
+{
+if (i == 98)
+{
+d2 = i % 10;
+d1 = i / 10;
+putchar(d1 + '0');
+putchar(d2 + '0');
+a = 0;
+break;
+}
+d2 = i % 10;
+d1 = i / 10;
+putchar(d1 + '0');
+putchar(d2 + '0');
+putchar(',');
+putchar(' ');
+}
+}
+if (a >= 100 && a < 1000)
+{
+for (i = a; i >= 100; i--)
+{
+if (i == 98)
+{
+d3 = i % 10;
+d2 = (i / 10) % 10;
+d1 = (i / 100) % 10;
+putchar(d1 + '0');
+putchar(d2 + '0');
+putchar(d3 + '0');
+break;
+}
+d3 = i % 10;
+d2 = (i / 10) % 10;
+d1 = (i / 100) % 10;
+putchar(d1 + '0');
+putchar(d2 + '0');
+putchar(d3 + '0');
+putchar(',');
+putchar(' ');
+}
+}
+if (a > 98 && a < 100 || i < 100 && i > 98)
+{
+a = i;
+for (i = a; i >= 98; i--)
+{
+if (i == 98)
+{
+d2 = i % 10;
+d1 = i / 10;
+putchar(d1 + '0');
+putchar(d2 + '0');
+break;
+}
+d2 = i % 10;
+d1 = i / 10;
+putchar(d1 + '0');
+putchar(d2 + '0');
+putchar(',');
+putchar(' ');
 }
 }
 return (0);
