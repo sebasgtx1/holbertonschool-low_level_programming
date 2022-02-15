bee68b9093ab4edc5cb6a3304ab9cf92b45a2169
SebasGTX1 <sebas1355840289@LAPTOP-1EFK09F7.localdomain>
2022-02-15 17:02:02 -0500

    Adv task 3 fixed

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,28 +1,27 @@
 #include <stdio.h>
 
 /**
- * main - Prints the add of the even-valued
- * fibonacci numbers.
+ * main - Prints + of the even-valued fibonacci numbers.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-long int n1, n2, fn, afn;
+long int f1, f2, fn, sumfnp;
 
-n1 = 1;
-n2 = 2;
-fn = afn = 0;
+f1 = 1;
+f2 = 2;
+fn = sumfnp = 0;
 while (fn <= 4000000)
 {
-fn = n1 + n2;
-n1 = n2;
-n2 = fn;
-if ((n1 % 2) == 0)
+fn = f1 + f2;
+f1 = f2;
+f2 = fn;
+if ((f1 % 2) == 0)
 {
-afn += n1;
+sumfnp += f1;
 }
 }
-printf("%ld\n", afn);
+printf("%ld\n", sumfnp);
 return (0);
 }
