fd4c9952cd5012b19224b9eca3ed2a9bb2f04cd1
SebasGTX1 <sebas1355840289@LAPTOP-1EFK09F7.localdomain>
2022-02-15 09:33:28 -0500

    task 5 fixed

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,7 +3,7 @@
 /**
  * print_sign -  checks sing of a number
  * @n: is a number
- * Return: 0 (Success) 1 (Fail)
+ * Return: 0 if n is zero 1 if n>0 and -1 if n<0
 */
 int print_sign(int n)
 {
@@ -18,11 +18,10 @@ if (n < 0)
 putchar(45);
 return (-1);
 }
-else ()
+if (n == 0)
 {
 putchar(48);
 return (0);
 }
 putchar('\n');
-}
 }
