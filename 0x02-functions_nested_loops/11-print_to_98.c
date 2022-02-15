28e2db8075ccefbe52d74be5908f792816d96fee
SebasGTX1 <sebas1355840289@LAPTOP-1EFK09F7.localdomain>
2022-02-14 21:15:00 -0500

    Task 11 fixed

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,120 +2,30 @@
 /**
  * print_to_98 - prints number until 98
  * @a: number
- * Return: 0 (Succes)
+ * Return: nothing
  */
 int print_to_98(int a)
 {
 int i, d1, d2, d3;
-i = 0;
-if (a <= -10)
+for (i = a; i < 98; i++)
 {
-for (i = a; i <= 98; i++)
-{
-if (i == 98)
-{
-d2 = i % 10;
-d1 = i / 10;
-putchar('-');
-putchar(d1 + '0');
-putchar(d2 + '0');
-break;
-}
-d2 = i % 10;
-d1 = i / 10;
-putchar('-');
-putchar(d1 + '0');
-putchar(d2 + '0');
-putchar(',');
-putchar(' ');
-}
-}
-if (a < 0 && a > -10)
-{
-for (i = a; i <= 98; i++)
-{
-if (i == 98)
-{
-putchar('-');
-putchar(i + '0');
-break;
-}
-putchar('-');
-putchar(-i + '0');
-putchar(',');
-putchar(' ');
-}
-}
-if (a >= 0 && a < 10)
-{
-for (i = a; i < 10; i++)
-{
-if (i == 98)
+if (i >= 0 && i < 10)
 {
 putchar(i + '0');
-break;
-}
-putchar(i + '0');
 putchar(',');
 putchar(' ');
-}
-}
-if (a >= 10 && a <= 98)
-{
-for (i = a; i <= 98; i++)
-{
-if (i == 98)
-{
-d2 = i % 10;
-d1 = i / 10;
-putchar(d1 + '0');
-putchar(d2 + '0');
-a = 0;
-i = 0;
-break;
-}
-d2 = i % 10;
-d1 = i / 10;
-putchar(d1 + '0');
-putchar(d2 + '0');
-putchar(',');
-putchar(' ');
-}
 }
 if (i >= 10 && i <= 98)
 {
-a = i;
-for (i = a; i <= 98; i++)
-{
-if (i == 98)
-{
-d2 = i % 10;
-d1 = i / 10;
-putchar(d1 + '0');
-putchar(d2 + '0');
-break;
-}
 d2 = i % 10;
 d1 = i / 10;
 putchar(d1 + '0');
 putchar(d2 + '0');
 putchar(',');
 putchar(' ');
-}
 }
 if (a > 98 && a < 100 || i < 100 && i > 98)
 {
-for (i = a; i >= 98; i--)
-{
-if (i == 98)
-{
-d2 = i % 10;
-d1 = i / 10;
-putchar(d1 + '0');
-putchar(d2 + '0');
-a = 0;
-break;
-}
 d2 = i % 10;
 d1 = i / 10;
 putchar(d1 + '0');
@@ -123,36 +33,6 @@ putchar(d2 + '0');
 putchar(',');
 putchar(' ');
 }
-}
-if (a >= 100 && a < 1000)
-{
-for (i = a; i >= 100; i--)
-{
-if (i == 98)
-{
-d3 = i % 10;
-d2 = (i / 10) % 10;
-d1 = (i / 100) % 10;
-putchar(d1 + '0');
-putchar(d2 + '0');
-putchar(d3 + '0');
-break;
-}
-d3 = i % 10;
-d2 = (i / 10) % 10;
-d1 = (i / 100) % 10;
-putchar(d1 + '0');
-putchar(d2 + '0');
-putchar(d3 + '0');
-putchar(',');
-putchar(' ');
-}
-}
-if (a > 98 && a < 100 || i < 100 && i > 98)
-{
-a = i;
-for (i = a; i >= 98; i--)
-{
 if (i == 98)
 {
 d2 = i % 10;
@@ -161,13 +41,5 @@ putchar(d1 + '0');
 putchar(d2 + '0');
 break;
 }
-d2 = i % 10;
-d1 = i / 10;
-putchar(d1 + '0');
-putchar(d2 + '0');
-putchar(',');
-putchar(' ');
 }
-}
-return (0);
 }
