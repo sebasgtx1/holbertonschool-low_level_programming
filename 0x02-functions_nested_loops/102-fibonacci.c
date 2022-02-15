998ab10b052926460c4ffe033cc22c249f57a510
SebasGTX1 <sebas1355840289@LAPTOP-1EFK09F7.localdomain>
2022-02-15 16:59:36 -0500

    Adv task 2 fixed

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 
 /**
- * main - Prints the Fibonacci numbers
+ * main - Prints the Fibonacci secuence
  *
  * Return: Always 0.
  */
 int main(void)
 {
 int c;
-long int n1, n2, fn;
+long int f1, f2, fn;
 
-n1 = 1;
-n2 = 2;
-printf("%ld, %ld", n1, n2);
+f1 = 1;
+f2 = 2;
+printf("%ld, %ld", f1, f2);
 for (c = 0; c < 48; c++)
 {
-fn = n1 + n2;
+fn = f1 + f2;
 printf(", %ld", fn);
-n1 = n2;
-n2 = fn;
+f1 = f2;
+f2 = fn;
 }
 printf("\n");
 return (0);
