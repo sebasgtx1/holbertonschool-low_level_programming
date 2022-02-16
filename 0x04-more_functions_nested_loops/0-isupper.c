6da5f5cf5673a683b5acef95b4b6a3c2413e63d2
SebasGTX1 <sebas-0202@hotmail.es>
2022-02-16 11:21:05 -0500

    Task 0 complete and check

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,1 +1,17 @@
-a
+#include "main.h"
+
+/**
+ * _isupper -  checks for uppercase character
+ * @c: is a letter
+ * Return: 1 if c is uppercase or 0 if is otherwise
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+	return (1);
+	}
+	else
+		return (0);
+}
