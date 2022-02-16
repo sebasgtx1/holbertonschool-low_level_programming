c17c9e1886d6b60f75c495730a03a28bea285940
SebasGTX1 <sebas-0202@hotmail.es>
2022-02-16 13:45:57 -0500

    Task 7 complete and check

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * print_diagonal -  draws a straight line in the terminal
+ * @n: number of lines to be printed
+ * Return: no return
+ */
+void print_diagonal(int n)
+{
+	int i, i2;
+
+	for (i = 1; i <= n; i++)
+	{
+		for (i2 = 1; i2 < i; i2++)
+		{
+		_putchar(32);
+
+		}
+
+	if (n > 0)
+	{
+	_putchar(92);
+	}
+	_putchar('\n');
+}
+
+if (n == 0)
+{
+_putchar('\n');
+}
+}
