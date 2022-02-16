fe9425b1ec16ee2b2ad7b0bb649105a4e2ef4c34
SebasGTX1 <sebas-0202@hotmail.es>
2022-02-16 13:19:01 -0500

    Task 6 complete and check

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_line -  draws a straight line in the terminal
+ * @n: number of lines to be printed
+ * Return: no return
+ */
+void print_line(int n)
+{
+	int i;
+	
+	for (i = 1; i <= n; i++)
+	{
+	_putchar(95);
+	}
+_putchar('\n');
+}
