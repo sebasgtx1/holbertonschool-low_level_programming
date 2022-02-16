a2f8057109b86e22eaee91b3fede07a1917bab8c
SebasGTX1 <sebas-0202@hotmail.es>
2022-02-16 16:07:47 -0500

    Task 9 complete and check

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * main - prints the numbers from 1 to 100
+ *  multiples of three print Fizz
+ *  multiples of five print Buzz
+ *  multiples of both three and five print FizzBuzz
+ *  Return: 0 (Succecs)
+ */
+
+int main(void)
+{
+int i;
+
+	for (i = 0; i <= 100;i++)
+	{
+		if (i % 3 != 0 && i % 5 != 0)
+		{
+			printf("%d ",i);
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+	}
+return (0);
+}
