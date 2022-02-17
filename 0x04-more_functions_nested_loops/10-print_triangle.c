a1740b6381372a6c3e3bb9535c8ddf5ae16fb10e
SebasGTX1 <sebas-0202@hotmail.es>
2022-02-17 16:30:21 -0500

    Task 10 fixed

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -9,13 +9,13 @@ void print_triangle(int size)
 {
 	int i, i2, i3;
 
-	for (i = 1; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
 		for (i2 = 1; i2 < size - i; i2++)
 		{
 			_putchar(32);
 		}
-		for (i3 = 1; i3 <= size - i2; i3++)
+		for (i3 = 0; i3 <= size - i2; i3++)
 		{
 			_putchar(35);
 		}
@@ -27,7 +27,6 @@ void print_triangle(int size)
 	}
 	if (size <= 0)
 	{
-	_putchar(35);
 	_putchar('\n');
 	}
 }
