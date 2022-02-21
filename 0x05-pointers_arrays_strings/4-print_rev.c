ef73c46f29a9e8202d0f8e1555a6d8af4fd3d762
SebasGTX1 <sebas-0202@hotmail.es>
2022-02-21 12:49:30 -0500

    Task 4 complete and check with the main.c file

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_rev - prints a string, in reverse, followed by a new line
+ * @s: string
+ * Return: no return
+ */
+
+void print_rev(char *s)
+{
+
+int i = 0;
+	while (*(s + i) != '\0')
+	{
+	i++;
+	}
+	while (i >= 0)
+	{
+	_putchar(*(s + i));
+	i--;
+	}
+_putchar('\n');
+}
