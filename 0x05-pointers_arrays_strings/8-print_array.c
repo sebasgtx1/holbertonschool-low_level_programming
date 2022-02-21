917e1b621c741608d18dac7bd4cb8fa5c047f37f
SebasGTX1 <sebas-0202@hotmail.es>
2022-02-21 15:22:53 -0500

    Task 8 complete and check withthe main.c file

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -8,11 +8,15 @@
  */
 void print_array(int *a, int n)
 {
-int i = 0;
-	printf("%d", *(a + i));
-	for (i = 1; i < n; i++)
+	int i;
+	
+	for (i = 0; i < n; i++)
 	{
-	printf(", %d", *(a + i));
+	printf("%d", *(a + i));
+	if (i != (n - 1))
+	{
+	printf(", ");
 	}
-_putchar('\n');
+	}
+	printf("\n");
 }
