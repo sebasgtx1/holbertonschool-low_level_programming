cbdc99295de4b5aec3886091f357bc61f86765de
SebasGTX1 <sebas-0202@hotmail.es>
2022-02-21 20:44:18 -0500

    Adv Task 2 complete and check

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,18 +1,24 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.h>
 /**
- * main - give a random key
- * Return: no return
+ * main - generates keygen.
+ * Return: 0 Always.
  */
 int main(void)
 {
-	int n;
+	int r = 0, c = 0;
+	time_t t;
 
-	srand(time(NULL));
-	n = rand();
-	
-	printf("%d", n);
-return(0);
-	
+	srand((unsigned int) time(&t));
+	while (c < 2772)
+	{
+		r = rand() % 128;
+		if ((c + r) > 2772)
+			break;
+		c = c + r;
+		printf("%c", r);
+	}
+	printf("%c\n", (2772 - c));
+	return (0);
 }
