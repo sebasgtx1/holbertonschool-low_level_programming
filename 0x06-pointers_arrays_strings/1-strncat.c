e39314ca276537e88d9beffb503f4396bcb1f844
SebasGTX1 <sebas-0202@hotmail.es>
2022-02-23 14:47:38 -0500

    Task 1 complete and check

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates a string with the n bytes of another one
+ * @src: string.
+ * @dest: string.
+ * Return: dest string.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+int j = 0;
+int i = 0;
+int k = 0;
+
+	while (*(dest + i) != '\0')
+	{
+		i++;
+	}
+
+	while (*(src + j) != '\0')
+	{
+		j++;
+	}
+
+	for (k = i; k < (i + n); k++)
+	{
+		if (k > j + i)
+		{
+		break;
+		}
+	*(dest + k) = src[k - i];
+		
+	}
+return (dest);
+}
