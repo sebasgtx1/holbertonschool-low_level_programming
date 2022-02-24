55e4728f92fa0d80dae4b5b1878331d1b86fb447
SebasGTX1 <sebas-0202@hotmail.es>
2022-02-24 07:56:04 -0500

    Task 4 fixed (Using a keep variable instead r[1000])

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -10,14 +10,13 @@ void reverse_array(int *a, int n)
 {
 int i, j, keep;
 
-    for (i = 0; i < n - 1; i++)
-    {
-        for (j = i + 1; j > 0; j--)
-        {
-            keep = *(a + j);
-            *(a + j) = *(a + (j - 1));
-            *(a + (j - 1)) = keep;
-        }
-    }
-
+	for (i = 0; i < n - 1; i++)
+	{
+		for (j = i + 1; j > 0; j--)
+		{
+			keep = *(a + j);
+			*(a + j) = *(a + (j - 1));
+			*(a + (j - 1)) = keep;
+		}
+	}
 }
