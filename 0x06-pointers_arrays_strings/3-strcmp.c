1d27f6a182a39ccd023f98fd8675d5e599fb1fef
SebasGTX1 <sebas-0202@hotmail.es>
2022-02-24 08:18:00 -0500

    Task 3 return fix (the integrers of the comparation have to be different)

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,29 +9,17 @@
 int _strcmp(char *s1, char *s2)
 {
 
-int j = 0;
 int i = 0;
 int k = 0;
 
-	while (*(s1 + i) != '\0')
+	while (k == 0)
 	{
-		i++;
+		if (*(s1 + i) == '\0' && *(s2 + i) == '\0')
+		{
+			break;
+		}
+	k = *(s1 + (i)) - *(s2 + (i));
+	i++;
 	}
-
-	while (*(s2 + j) != '\0')
-	{
-		j++;
-	}
-
-	if (i == j)
-	{
-		k = 0;
-	}
-
-	if (i > j || i < j)
-	{
-		k = *(s1 + (i - 1)) - *(s2 + (j - 1));
-	}
-	
 return (k);
 }
