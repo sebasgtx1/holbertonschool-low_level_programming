69c365780368390c691d9c277b4704546f23d54e
SebasGTX1 <sebas-0202@hotmail.es>
2022-02-24 07:41:36 -0500

    Adv task 2 complete and check with main.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * print_number - prints an integer
+ * @n: integer
+ * Return: no return
+ */
+void print_number(int n)
+
+{
+unsigned int i, d, c;
+
+	if (n < 0)
+	{
+		_putchar(45);
+		i = n * -1;
+	}
+	else
+	{
+		i = n;
+	}
+
+	d = i;
+	c = 1;
+
+	while (d > 9)
+	{
+		d /= 10;
+		c *= 10;
+	}
+
+	for (; c >= 1; c /= 10)
+	{
+		_putchar(((i / c) % 10) + 48);
+	}
+}
