8a475b917b52b12cb8672bc38a404658d643b2df
SebasGTX1 <sebas-0202@hotmail.es>
2022-02-24 15:02:56 -0500

    Adv task 5, only creation of the void file

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_buffer - prints a buffer (b)
+ * @b: buffer.
+ * @size: size of buffer.
+ * Return: no return.
+ */
+void print_buffer(char *b, int size)
+{
+
+}
