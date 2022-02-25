c5c0771c4a69b8111d49d9dacb71423918a2b295
SebasGTX1 <sebas-0202@hotmail.es>
2022-02-24 20:12:23 -0500

    Adv Task 5 complete (in python tutor first like most of times), and check with the main.c file

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
  * print_buffer - prints a buffer (b)
  * @b: buffer.
@@ -9,10 +8,35 @@
  */
 void print_buffer(char *b, int size)
 {
-int s;
+	int j, k, l;
 
-*b = 0;
-size = 0;
-s = 1;
-*b = size + s;
+	if (size <= 0)
+		printf("\n");
+	else
+	{
+		for (j = 0; j < size; j += 10)
+		{
+			printf("%.8x:", j);
+			for (k = j; k < j + 10; k++)
+			{
+				if (k % 2 == 0)
+					printf(" ");
+				if (k < size)
+					printf("%.2x", *(b + k));
+				else
+					printf("  ");
+			}
+		printf(" ");
+		for (l = j; l < j + 10; l++)
+		{
+			if (l >= size)
+				break;
+			if (*(b + l) < 32 || *(b + l) > 126)
+				printf("%c", '.');
+			else
+				printf("%c", *(b + l));
+		}
+		printf("\n");
+		}
+	}
 }
