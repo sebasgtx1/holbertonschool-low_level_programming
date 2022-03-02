c6c1111cafd5b55936401ac1f0608467a5497c23
SebasGTX1 <sebas-0202@hotmail.es>
2022-03-02 12:17:08 -0500

    Task 4 complete and check with main.c and betty style

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - returns the value of x raised to the power of y
+ * @x: base
+ * @y: power
+ * Return: x ^ y
+ */
+
+int _pow_recursion(int x, int y)
+{
+if (y == 0)
+{
+return (1);
+}
+if (y < 0)
+{
+return (-1);
+}
+y--;
+return (x * _pow_recursion(x, y));
+}
