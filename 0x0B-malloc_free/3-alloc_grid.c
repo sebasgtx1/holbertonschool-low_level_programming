65c378dc4f67bf6db4b4d128097666ce27e89608
SebasGTX1 <sebas-0202@hotmail.es>
2022-03-07 13:45:24 -0500

    Correction to the asignation of memory, the height asignation is int* because es 2 dimensonal array

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -17,7 +17,7 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	alloc = malloc(sizeof(int) * height);
+	alloc = malloc(sizeof(int *) * height);
 
 	if (alloc == NULL)
 	{
