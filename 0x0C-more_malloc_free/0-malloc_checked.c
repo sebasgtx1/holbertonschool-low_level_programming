b833b783a12f06a066bf8ba6d1ae10c6a326504c
SebasGTX1 <sebas-0202@hotmail.es>
2022-03-10 10:25:59 -0500

    Task 0 complete and check with main.c (betty ok)

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * malloc_checked - allocates memory
+ * @b: amount of bytes
+ * Return: pointer to the allocated memory
+ * (if malloc fails, the exit status value is 98)
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	char *memory;
+
+	memory = malloc(b);
+
+	if (memory == NULL)
+		exit(98);
+
+	return (memory);
+
+}
