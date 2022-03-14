58e0a0a210b261d4e990c56b97bc69a7eaa91e77
SebasGTX1 <sebas-0202@hotmail.es>
2022-03-14 13:23:40 -0500

    Update header file with all prototypes

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,11 @@
 #ifndef _dog_h_
 #define _dog_h_
 
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
 /**
  * struct dog - struct that stores some information of a dog
  * @name: name of the dog
