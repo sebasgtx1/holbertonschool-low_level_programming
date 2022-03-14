0123a8c8cc38361843519207f9f7553d917d46c1
SebasGTX1 <sebas-0202@hotmail.es>
2022-03-14 13:37:13 -0500

    Task 1 and UNupdating the header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,6 @@
 #ifndef _dog_h_
 #define _dog_h_
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
 
 /**
  * struct dog - struct that stores some information of a dog
@@ -21,4 +17,5 @@ struct dog
 	char *owner;
 };
 
+void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif
