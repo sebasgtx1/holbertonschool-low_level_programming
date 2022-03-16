1ed6a1b74bcae802bab858157fab60a854dc7a12
SebasGTX1 <sebas-0202@hotmail.es>
2022-03-16 13:12:45 -0500

    Main task 3

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,29 @@
+#include "3-calc.h"
+
+/**
+ * main - check the code
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int result;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	if (argv[2][1])
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	result = get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3]));
+
+	printf("%d\n", result);
+	return (0);
+}
