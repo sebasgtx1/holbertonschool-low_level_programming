665ab062b4c8b372c7542af84e0356dd545dd7bf
SebasGTX1 <sebas-0202@hotmail.es>
2022-03-16 08:55:06 -0500

    Task 0 complete and check with main and betty

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name
+ * @name: name to be printed
+ * @f: function pointer
+ * Return: no return
+ */
+void print_name(char *name, void (*f)(char *))
+{
+		f(name);
+}
