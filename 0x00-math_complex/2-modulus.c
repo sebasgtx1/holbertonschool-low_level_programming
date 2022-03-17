bb076f5a4f0f461fd49499787c1fac2047ff5b4b
SebasGTX1 <sebas-0202@hotmail.es>
2022-03-17 12:03:19 -0500

    Task 2 done, betty check (-lm it has to be at the final step of gcc, the checker must be revised)

---
+++ 0x00-math_complex/2-modulus.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * modulus - calculate and return the modulus of
+ * a given complex number
+ * @c: struct complex
+ * Return: a double (modulus)
+ */
+
+double modulus(complex c)
+{
+        double mod;
+
+        mod = (double) sqrt((pow(c.re, 2) + pow(c.im, 2)));
+
+        return (mod);
+
+}
