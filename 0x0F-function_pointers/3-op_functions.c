d34406e03f119b15edd90cf263fcc78671f89016
SebasGTX1 <98430593+SebasGTX1@users.noreply.github.com>
2022-03-17 10:07:07 -0500

    Fixing the exit values

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -44,7 +44,7 @@ int op_div(int a, int b)
 	if (b == 0)
 	{
 		printf("Error\n");
-		exit(0);
+		exit(100);
 	}
 	return (a / b);
 }
@@ -60,7 +60,7 @@ int op_mod(int a, int b)
 	if (b == 0)
 	{
 		printf("Error\n");
-		exit(0);
+		exit(100);
 	}
 	return (a % b);
 }
