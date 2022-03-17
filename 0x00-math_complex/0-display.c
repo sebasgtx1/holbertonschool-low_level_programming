5713dee49a87978342c7d106a4718a1bf823e852
SebasGTX1 <sebas-0202@hotmail.es>
2022-03-16 20:02:14 -0500

    Task 0 complete and check with main and betty

---
+++ 0x00-math_complex/0-display.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * display_complex_number - Display a complex number
+ * @c: struct complex
+ * Return: no return
+ */
+
+void display_complex_number(complex c)
+{
+
+		if (c.im < 0)
+			printf("%d - %di\n", c.re, -c.im);
+		if (c.im == 0)
+			printf("%d\n", c.re);
+		else
+			printf("%d + %di\n", c.re, c.im);
+
+
+}
