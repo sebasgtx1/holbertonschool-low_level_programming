6ec0794941eaaf8e44ae93aebf615c7aed058bb6
SebasGTX1 <sebas-0202@hotmail.es>
2022-03-28 14:38:49 -0500

    Task 2 complete (betty (ok) main.c (ok))

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+/**
+ * add_node - adds a new node at the beginning
+ * of a list_t list.
+ * @head: head of the linked list.
+ * @str: string to store in the list.
+ * Return: address of the head.
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_h;
+	size_t strlen = 0;
+
+	new_h = malloc(sizeof(list_t));
+	if (!new_h)
+		return (NULL);
+	
+	new_h->str = strdup(str);
+	for (; str[strlen]; strlen++)
+		;
+	new_h->len = strlen;
+	new_h->next = *head;
+	*head = new_h;
+	return (*head);
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -2,6 +2,8 @@
 #define _lists_
 #include <stddef.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
