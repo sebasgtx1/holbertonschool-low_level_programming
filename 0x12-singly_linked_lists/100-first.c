f883e31b7f5dbddaa45f072d4a4d80a268fad9f1
SebasGTX1 <sebas-0202@hotmail.es>
2022-03-29 08:36:24 -0500

    ADV task complete (betty (ok) main.c (ok))

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * print_before_main - prints something before main
+ *
+ * Return: no return
+ */
+
+void print_before_main(void)
+{
+	printf("You're beat! and yet, you must allow,");
+	printf("\nI bore my house upon my back!\n");
+}
