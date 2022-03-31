b4683faded6358505a42565bde60166f9fbd1866
SebasGTX1 <sebas-0202@hotmail.es>
2022-03-31 07:45:34 -0500

    Task 0 complete (betty (ok) main (ok))

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * print_listint - prints all the elements
+ * of a listint_t list
+ * @h: linked list to be printed
+ * Return: the number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t nodes = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		nodes++;
+	}
+	return (nodes);
+}
