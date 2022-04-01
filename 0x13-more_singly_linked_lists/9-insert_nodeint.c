331dc289fcf80075cd7d13bd9c842586b61f4901
SebasGTX1 <sebas-0202@hotmail.es>
2022-03-31 20:53:27 -0500

    Pushing incomplete file for cloning in another pc

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * insert_nodeint_at_index -  inserts a new node at a given position
+ * @head: pinter to the head list
+ * @idx:index of the list where the new node should be added.
+ * Index starts at 0
+ * @n: number to be add to the index node
+ * Return: the address of the new node, or NULL if it failed
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *newn, *temp;
+	unsigned int index = 0;
+	
+	temp = *head;
+
+}
