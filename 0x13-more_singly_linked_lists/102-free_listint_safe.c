aed7c33d78ac826238b37f58b0ebe55b1f0c39bb
SebasGTX1 <sebas-0202@hotmail.es>
2022-04-03 14:12:08 -0500

    adv task 3 complete (betty (ok) main (ok))

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,67 @@
+#include "lists.h"
+/**
+ * free_listp3 - frees a listint_t list and set the head to NULL
+ * @head: pinter to the list to be free it
+ * Return: no return
+ */
+void free_listp3(list_p **head)
+{
+	list_p *headp;
+
+	if (head)
+	{
+	while ((headp = *head))
+	{
+		*head = (*head)->next;
+		free(headp);
+	}
+	*head = NULL;
+	}
+}
+/**
+ * free_listint_safe - frees a linked list.
+ * @h: head of a list.
+ *
+ * Return: size of the list that was freed.
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	size_t nodes = 0;
+	list_p *a_u, *a_h, *a_c;
+	listint_t *hp;
+
+	a_u = NULL;
+	while (*h)
+	{
+		a_h = malloc(sizeof(list_p));
+
+		if (!a_c)
+			exit(98);
+
+		a_h->address = (void *)*h;
+		a_h->next = a_u;
+		a_u = a_h;
+
+		a_c = a_u;
+
+		while (a_c->next)
+		{
+			a_c = a_c->next;
+			if (*h == a_c->address)
+			{
+				*h = NULL;
+				free_listp3(&a_u);
+				return (nodes);
+			}
+		}
+
+		hp = *h;
+		*h = (*h)->next;
+		free(hp);
+		nodes++;
+	}
+
+	*h = NULL;
+	free_listp3(&a_u);
+	return (nodes);
+}
