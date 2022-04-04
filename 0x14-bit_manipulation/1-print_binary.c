dde1c29bedc494e43ba0616015bba41b2c95b8bd
SebasGTX1 <sebas-0202@hotmail.es>
2022-04-04 15:21:13 -0500

    Using the >> operator

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -6,15 +6,13 @@
  */
 void print_binary(unsigned long int n)
 {
-	unsigned long int div = 0, bol = 0;
-
-	while (n >= 2)
+	if (n >> 0 != 0)
 	{
-		n = n - 2;
-		div++;
-		bol = 1;
+
+		if (n >> 1 != 0)
+			print_binary(n >> 1);
+		_putchar((n & 1) + '0');
 	}
-	if (bol == 1)
-		print_binary(div);
-	 _putchar (n + '0');
+	else
+		_putchar ('0');
 }
