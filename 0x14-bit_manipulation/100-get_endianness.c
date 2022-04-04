9ce12c4cfef47d3ab7e56db92fbf2cd7a21fb830
SebasGTX1 <sebas-0202@hotmail.es>
2022-04-04 18:15:26 -0500

    adv task 1 complete (betty (ok) main (ok))

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * get_endianness - checks the endianness
+ *
+ * Return: 0 if big endian, 1 if little endian
+ */
+int get_endianness(void)
+{
+	unsigned int x = 0x76543210;
+	char *c = (char *) &x;
+
+	if (*c == 0x10)
+		return (1);
+	else
+		return (0);
+}
