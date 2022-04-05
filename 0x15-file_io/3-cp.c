c94b7c72823cb06bdb1d54c7e1429f3d0006a169
SebasGTX1 <sebas-0202@hotmail.es>
2022-04-05 17:56:46 -0500

    Fixing errors form sintax error

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -22,7 +22,7 @@ int main(int argc, char **argv)
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 	if (read_val == -1)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
-	if (close(f_form) == -1)
+	if (close(f_from) == -1)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f_from), exit(100);
 	if (close(f_to) == -1)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f_to), exit(100);
