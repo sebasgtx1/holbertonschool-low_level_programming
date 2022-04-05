0f8ab46b0ff5e5b13243a6b543bce09a36a7e2c2
SebasGTX1 <sebas-0202@hotmail.es>
2022-04-05 18:00:29 -0500

    Finally...

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -15,13 +15,19 @@ int main(int argc, char **argv)
 		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to"), exit(97);
 	f_from = open(argv[1], O_RDONLY);
 	if (f_from == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
 	f_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	while ((read_val = read(f_from, buff, 1024)) > 0)
 		if (f_to == -1 || read_val != write(f_to, buff, read_val))
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 	if (read_val == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
 	if (close(f_from) == -1)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f_from), exit(100);
 	if (close(f_to) == -1)
