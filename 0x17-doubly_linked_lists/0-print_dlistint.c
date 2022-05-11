da6d764280287ffca6a311b52b627a658487801e
SebasGTX1 <sebas-0202@hotmail.es>
2022-05-11 06:59:23 -0500

    Task 0 complete and check with betty and main

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - prints all the elements of
+ * a doubly linked list
+ * @h: linked list
+ * Return: number of nodes
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t nodes = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		nodes++;
+	}
+	return (nodes);
+}
