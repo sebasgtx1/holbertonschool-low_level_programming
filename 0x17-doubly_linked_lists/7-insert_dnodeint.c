5e7022c2295126a400d4a22838ea11784903c9de
SebasGTX1 <sebas-0202@hotmail.es>
2022-05-12 07:02:11 -0500

    Fix: case - intert node at the last index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -32,6 +32,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		(*h)->prev = newn;
 		*h = newn;
 	}
+	else if (!temp->next)
+	{
+		newn = add_dnodeint_end(h, n);
+	}
 	else
 	{
 		newn->next = temp->next;
