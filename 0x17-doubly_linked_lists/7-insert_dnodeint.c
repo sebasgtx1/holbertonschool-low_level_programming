e9c376a42204f7c05f9c5438a4079e51eac5ac9a
SebasGTX1 <sebas-0202@hotmail.es>
2022-05-12 09:53:25 -0500

    Fix: out of range index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,5 +1,22 @@
 #include "lists.h"
 /**
+ * dlistint_len - returns the number of elements
+ * of a doubly linked list
+ * @h: linked list
+ * Return: the number of nodes
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t nodes = 0;
+
+	while (h)
+	{
+		h = h->next;
+		nodes++;
+	}
+	return (nodes);
+}
+/**
  * insert_dnodeint_at_index -  inserts a new node at a given position
  * @h: pinter to the head list
  * @idx:index of the list where the new node should be added.
@@ -21,18 +38,16 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if ((int)idx > 0 && temp)
 	{
 		for (; index < idx - 1 && temp; index++)
-		{
-			size++;
 			temp = temp->next;
-		}
 	}
+	size = dlistint_len(*h);
 	newn = malloc(sizeof(dlistint_t));
 	if (!newn)
 		return (NULL);
 	newn->n = n;
-	if (idx == 0 || !temp)
+	if (idx == 0)
 		return (add_dnodeint(h, n));
-	else if (idx <= size)
+	else if (idx >= size)
 	{
 		return (add_dnodeint_end(h, n));
 	}
