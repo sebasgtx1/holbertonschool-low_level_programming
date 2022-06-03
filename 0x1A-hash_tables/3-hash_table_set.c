9c75fa3b7e743180b020a9d8ac5722878ff541f5
SebasGTX1 <sebas-0202@hotmail.es>
2022-06-03 16:18:56 -0500

    Task 3 complete

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,43 @@
+#include "hash_tables.h"
+#include <string.h>
+#include <stdio.h>
+/**
+ * hash_table_set - function that adds an element to the hash table
+ * @ht: has table
+ * @key: key
+ * @value: value is the value associated with the key
+ * Return: 1 if it succeeded, 0 otherwise
+ * In case of collision, add the new node at the beginning of the list
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *temp;
+	unsigned long int key_i;
+
+	key_i = key_index((unsigned char *)key, ht->size);
+	temp = ht->array[key_i];
+
+	while (temp)
+	{
+		/* Update the value*/
+		if (strcmp(temp->key, key) == 0)
+		{
+			free(temp->value);
+			temp->value = strdup(value);
+		}
+		/* going throught the table*/
+		temp = temp->next;
+
+	}
+	/* new node has to be created with the given attributes*/
+	/* because the key does not exist or because is a collision case */
+	temp = malloc(sizeof(hash_node_t));
+	if (!temp)
+		return (0);
+	temp->key = strdup(key);
+	temp->value = strdup(value);
+	temp->next =  ht->array[key_i];
+	ht->array[key_i] = temp;
+	return (1);
+}
