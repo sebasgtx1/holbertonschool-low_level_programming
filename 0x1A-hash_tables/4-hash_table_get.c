8bb6cb8c9ed8ed0979f783c4bb93dd2f4c762e65
SebasGTX1 <sebas-0202@hotmail.es>
2022-06-03 21:56:56 -0500

    Task 4 complete

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+/**
+ * hash_table_get - function that retrieves a value associated with a key
+ * @ht: hash table
+ * @key: key to be get
+ * Return: the value associated with the element
+ * or NULL if key couldn’t be found
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	hash_node_t *temp;
+	unsigned long int index;
+
+	index = key_index((unsigned char *)key, ht->size);
+	temp = ht->array[index];
+	while (temp)
+	{
+		if (strcmp(temp->key, key) == 0)
+			break;
+		temp = temp->next;
+	}
+	if (!temp)
+		return (NULL);
+	return (temp->value);
+
+}
