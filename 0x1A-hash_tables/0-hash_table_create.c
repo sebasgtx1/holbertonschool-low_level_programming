b481b15e6122e77543eb46bb96065817e2159004
SebasGTX1 <sebas-0202@hotmail.es>
2022-06-05 12:36:39 -0500

    Fix: the nodes has to be pointers

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -17,7 +17,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (!my_table)
 		return (NULL);
 
-	array = malloc(size * (sizeof(hash_node_t)));
+	array = malloc(size * (sizeof(hash_node_t *)));
 	if (!array)
 		return (NULL);
 
