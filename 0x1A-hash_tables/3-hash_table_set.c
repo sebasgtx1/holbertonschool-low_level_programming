79f7213f12127ccba51e2f045bc4f0cf950d1d63
SebasGTX1 <sebas-0202@hotmail.es>
2022-06-05 13:34:29 -0500

    Revert "Value could be NULL?"
    This reverts commit e6633d3eceabe4bd72efddbc73079e8ee1db62c1.

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -15,7 +15,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *temp;
 	unsigned long int key_i;
 
-	if (!ht || !key)
+	if (!ht || !key || !value)
 		return (0);
 	if (*key == '\0')
 		return (0);
