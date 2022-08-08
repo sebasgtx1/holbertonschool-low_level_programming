5546705244c89977fa4e9c5ca08b4d73beebe3df
SebasGTX1 <sebas-0202@hotmail.es>
2022-08-08 10:57:38 -0500

    Adding header file

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,12 @@
+#ifndef _SEARCH_ALGOS_H
+#define _SEARCH_ALGOS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+#endif /* _SEARCH_ALGOS_H */
+
+
